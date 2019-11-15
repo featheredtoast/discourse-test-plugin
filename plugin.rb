7a0ab9469e959d9b2baab4d5f7b7324628e1f993
Jeff Wong <awole20@gmail.com>
2019-11-15 15:19:19 -0800

    Add basic plugin

---
+++ plugin.rb
@@ -0,0 +1,8 @@
+# name: discourse-test-plugin
+# about: Test plugin
+# version: 0.1.0
+# authors: Jeff Wong
+# url: https://github.com/featheredtoast/discourse-test-plugin
+
+after_initialize do
+end
